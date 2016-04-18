1cfcd121a1672518e5baeee2dc0888c293ff3f34
Stephanie Unna <steph.unna@gmail.com>
2016-04-18 11:19:10 +0100

    Updating ordering of elements to print simple map of Africa.

--- scripts/mapestry.js
+++ scripts/mapestry.js
@@ -5,14 +5,18 @@ var svg = d3.select("#map-container").append("svg")
                                      .attr("width", width)
                                      .attr("height", height);
 
-var subunits = topojson.feature(africa, africa.objects.continenttest);
-
-
-
 d3.json("africa.json", function(error, africa) {
   if (error) return console.error(error);
 
+  var countries = topojson.feature(africa, africa.objects.continenttest);
+
+  var projection = d3.geo.mercator()
+                         .scale(500)
+                         .translate([width / 2, height / 2]);
+
+  var path = d3.geo.path().projection(projection)
+
   svg.append("path")
-     .datum(topojson.feature(africa, africa.objects.continenttest))
-     .attr("d", d3.geo.path().projection(d3.geo.mercator()));
+     .datum(countries)
+     .attr("d", path);
 });
