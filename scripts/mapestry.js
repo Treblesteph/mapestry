c0ff356be30de224a2f78789dbb962af392033b5
Stephanie Unna <steph.unna@gmail.com>
2016-06-21 10:34:42 +0100

    Changing Africa projection to azimuthal equal area.

--- scripts/mapestry.js
+++ scripts/mapestry.js
@@ -1,22 +1,22 @@
-var width = 960,
-    height = 1160;
+var width = 960
+var height = 1160
 
 var svg = d3.select("#map-container").append("svg")
                                      .attr("width", width)
-                                     .attr("height", height);
+                                     .attr("height", height)
 
 d3.json("africa.json", function(error, africa) {
-  if (error) return console.error(error);
+  if (error) return console.error(error)
 
-  var countries = topojson.feature(africa, africa.objects.continenttest);
+  var countries = topojson.feature(africa, africa.objects.continenttest)
 
-  var projection = d3.geo.mercator()
+  var projection = d3.geo.azimuthalEqualArea()
                          .scale(500)
-                         .translate([width / 2, height / 2]);
+                         .translate([width / 2, height / 2])
 
   var path = d3.geo.path().projection(projection)
 
   svg.append("path")
      .datum(countries)
-     .attr("d", path);
-});
+     .attr("d", path)
+})
