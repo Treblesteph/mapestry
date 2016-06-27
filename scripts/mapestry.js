70d9d676f9cb908eda163c14115f5e579322d476
Stephanie Unna <steph.unna@gmail.com>
2016-06-27 13:04:25 +0100

    Making a general show continent function that can be toggled using continents buttons.

--- scripts/mapestry.js
+++ scripts/mapestry.js
@@ -11,25 +11,50 @@ var svg = d3.select("#map-container").append("div")
                                      .attr("viewBox", "0 0 600 800")
                                      .classed("svg-content-responsive", true)
 
-d3.json("africa.json", function(error, africa) {
-  if (error) return console.error(error)
+var northamericaprojection = d3.geo.conicConformal()
+                                   .rotate([98, 0])
+                                   .center([0, 38])
+                                   .parallels([29.5, 45.5])
+                                   .scale(1000)
+                                   .translate([width / 2, height / 2])
+                                   .precision(.1)
+var southamericaprojection = d3.geo.azimuthalEqualArea()
+var europeprojection = d3.geo.conicConformal()
+var africaprojection = d3.geo.azimuthalEqualArea()
+                             .scale(350)
+                             .translate([width / 5, height / 4])
+var asiaprojection = d3.geo.conicConformal()
+var oceaniaprojection = d3.geo.orthographic()
+    // .scale(475)
+    // .translate([width / 2, height / 2])
+    // .clipAngle(90)
+    // .precision(.1)
 
-  var countries = topojson.feature(africa, africa.objects.continenttest)
+function showcontinent(continentname) {
+  var filename = continentname + ".json"
+  d3.json(filename, function(error, continent) {
+    if (error) return console.error(error)
 
-  var projection = d3.geo.azimuthalEqualArea()
-                         .scale(350)
-                         .translate([width / 5, height / 4])
+    var geofilename = "continent" + continentname
+    var countries = topojson.feature(continent, continent.objects[geofilename])
 
-  var path = d3.geo.path().projection(projection)
+    var projection = d3.geo.robinson()
+                           .scale(150)
+                           .translate([width / 2, height / 2])
+                           .precision(.1)
 
-  svg.append("path")
-     .datum(countries)
-     .attr("d", path)
+    var path = d3.geo.path().projection(projection)
 
-console.log(svg.selectAll(".subunit")
-     .data(topojson.feature(africa, africa.objects.continenttest).features)
-     .enter().append("path")
-     .attr("class", function(d) { return "africa " + d.id; })
-     .attr("d", path))
+    svg.append("path")
+       .datum(countries)
+       .attr("d", path)
 
-})
+    console.log(svg.selectAll(".subunit")
+           .data(topojson.feature(continent, continent.objects[geofilename]).features)
+           .enter().append("path")
+           .attr("class", function(d) { return continentname + " " + d.id; })
+           .attr("d", path))
+  })
+}
+
+showcontinent("southamerica")
